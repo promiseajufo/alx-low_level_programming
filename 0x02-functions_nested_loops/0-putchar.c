2affc0ed36e21f7607d7f566350601400da1e18a
promiseajufo <ajufopromise@gmail.com>
2023-04-26 11:44:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+  1 #include "main.h"
+  2 /**
+  3  * main - Entry points
+  4  * Description: 'prints _putchar to stdout'
+  5  * Return: always 0
+  6  */
+  7 int main(void)
+  8 {
+  9       _putchar('_');
+ 10       _putchar('p');
+ 11       _putchar('u');
+ 12       _putchar('t');
+ 13       _putchar('c');
+ 14       _putchar('h');
+ 15       _putchar('a');
+ 16       _putchar('r');
+ 17       _putchar('\n');
+ 18       return (0);
+ 19 }
