bd56937eb3445b3b8fb86d4e563beda5fa62e21c
promiseajufo <ajufopromise@gmail.com>
2023-04-28 15:44:22 -0700

    okay

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  * _isupper - checks for lowercase
+ *   * @c: The charcter to be checked
+ *    * Return: 1 for uppercase or 0 fr anyting else
+ *     */
+
+int _isupper(int c)
+{
+	        if (c >= 65 && c <= 90)
+			        {
+					                return (1);
+							        }
+		        return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * mul - muliplies tqow integers
+ *   * @a: the first integer
+ *    * @b: The seconf integer
+ *     * Return: 0 always
+ *      */
+
+int mul(int a, int b)
+{
+	        int result = a * b;
+
+		        return (result);
+}
