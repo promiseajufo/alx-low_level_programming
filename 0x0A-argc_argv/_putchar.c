10b27c87383618a06f00df882a0ea6b9b074ac5f
promiseajufo <ajufopromise@gmail.com>
2023-05-17 19:54:01 -0700

    k

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
+
+root@d9b9b29248ca:~/alx-low_level_programming/0x0A-argc_argv# cat _putchar.c
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c:Ther character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
